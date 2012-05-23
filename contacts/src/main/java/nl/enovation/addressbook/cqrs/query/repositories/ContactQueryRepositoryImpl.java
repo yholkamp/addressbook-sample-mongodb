9e2bd32e55dc4f1082f258095458ffd5c93ed016
Maarten van Meijeren <pmijdereend@gmail.com>
2012-05-23 16:35:53 +0200

    lost test added

--- contacts/src/main/java/nl/enovation/addressbook/cqrs/query/repositories/ContactQueryRepositoryImpl.java
+++ contacts/src/main/java/nl/enovation/addressbook/cqrs/query/repositories/ContactQueryRepositoryImpl.java
@@ -17,24 +17,46 @@ public class ContactQueryRepositoryImpl extends CFMongoTemplate implements Conta
         super(mongoDbFactory);
     }
 
+    /**
+     * Delete contactEntry equal to parameter
+     * 
+     * @param contactEntry contactEntry that has to be deleted 
+     */
     public void delete(ContactEntry contactEntry) {
         Criteria idCriterion = new Criteria("_id").is(contactEntry.getIdentifier());
         Query query = Query.query(idCriterion);
         super.remove(query, ContactEntry.class);
     }
 
+    /**
+     * delete alle contactEntries from the database
+     */
     public void deleteAll() {
         Criteria idCriterion = new Criteria();
         Query query = Query.query(idCriterion);
         super.remove(query, ContactEntry.class);
     }
 
+    /**
+     * find the contactEntry that has the identifier that is given by parameter
+     * 
+     * @param contactIdentifier identifier for contactEntry that has to be found
+     * 
+     * @return contactEntry with identifier equals to parameter
+     */
     public ContactEntry findOne(String contactIdentifier) {
         Criteria idCriterion = new Criteria("_id").is(contactIdentifier);
         Query query = Query.query(idCriterion);
         return super.findOne(query, ContactEntry.class);
     }
 
+    /**
+     * find List of contactEntries that contains (sub)string searchValue 
+     * 
+     * @param searchValue contains (sub)string that has to be found
+     * 
+     * @return List<ContactEntry> list of contactEntries that contain searchValue in first or lastName 
+     */
     @Override
     public List<ContactEntry> searchForNames(String searchValue) {
         searchValue = ".*" + searchValue + ".*";
---
+++ contacts/src/test/java/nl/enovation/addressbook/cqrs/query/repositories/ContactQueryRepositoryImplTest.java
@@ -0,0 +1,97 @@
+package nl.enovation.addressbook.cqrs.query.repositories;
+
+import java.util.List;
+
+import nl.enovation.addressbook.cqrs.query.ContactEntry;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/META-INF/spring/persistence-infrastructure-context.xml",
+                                   "classpath:/META-INF/spring/cqrs-infrastructure-context.xml", "classpath:/META-INF/spring/contacts-context.xml" })
+public class ContactQueryRepositoryImplTest {
+
+    @Autowired
+    private ContactQueryRepositoryImpl contactQueryRepositoryImpl;
+
+    private String searchValue;
+
+    private ContactEntry contactTest;
+
+    private ContactEntry contactTest2;
+
+    private String firstName = "Foo";
+
+    private String lastName = "Bar";
+
+    @After
+    public void after() {
+        contactQueryRepositoryImpl.deleteAll();
+    }
+
+    @Before
+    public void setup() {
+        contactTest = new ContactEntry();
+        contactTest.setFirstName(firstName);
+        contactTest.setLastName(lastName);
+
+        contactTest2 = new ContactEntry();
+        contactTest2.setFirstName(firstName);
+        contactTest2.setLastName(lastName);
+    }
+
+    @Test
+    public void testDelete() {
+        contactQueryRepositoryImpl.delete(contactTest);
+        assertNull(contactQueryRepositoryImpl.findOne(contactTest.getIdentifier()));
+    }
+
+    @Test
+    public void testDeleteAll() {
+        contactQueryRepositoryImpl.save(contactTest);
+        contactQueryRepositoryImpl.save(contactTest2);
+        contactQueryRepositoryImpl.deleteAll();
+        assertNull(contactQueryRepositoryImpl.findOne(contactTest.getIdentifier()));
+        assertNull(contactQueryRepositoryImpl.findOne(contactTest2.getIdentifier()));
+    }
+
+    @Test
+    public void testFindOneString() {
+        contactQueryRepositoryImpl.save(contactTest);
+        assertEquals(contactTest, contactQueryRepositoryImpl.findOne(contactTest.getIdentifier()));
+    }
+
+    @Test
+    public void testSearchForNames() {
+        contactQueryRepositoryImpl.deleteAll();
+
+        String randomString = "&*(@$&(⁹823jsf";
+
+        contactQueryRepositoryImpl.save(contactTest);
+
+        searchValue = contactTest.getFirstName();
+        List<ContactEntry> contacts = contactQueryRepositoryImpl.searchForNames(searchValue);
+        assertTrue(contacts.contains(contactTest));
+
+        searchValue = contactTest.getLastName();
+        contacts.clear();
+        contacts = contactQueryRepositoryImpl.searchForNames(searchValue);
+        assertTrue(contacts.contains(contactTest));
+
+        searchValue = randomString;
+        contacts.clear();
+        contacts = contactQueryRepositoryImpl.searchForNames(searchValue);
+        assertFalse(contacts.contains(contactTest));
+    }
+}
