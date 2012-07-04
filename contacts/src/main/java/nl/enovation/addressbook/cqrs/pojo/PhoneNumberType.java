c64370e57dc4c75d8a573e0e2165fba66e6a37a4
Yorick Holkamp <yorick@nextpulse.net>
2012-07-04 21:29:27 +0200

    Added userfriendly names to the PhoneNumberType enum

--- contacts/src/main/java/nl/enovation/addressbook/cqrs/pojo/PhoneNumberType.java
+++ contacts/src/main/java/nl/enovation/addressbook/cqrs/pojo/PhoneNumberType.java
@@ -4,5 +4,26 @@ package nl.enovation.addressbook.cqrs.pojo;
  * Enum to represent the different types of PhoneNumber entries we identify in our system.
  */
 public enum PhoneNumberType {
-    HOME, WORK, FAX, OTHER
+    HOME("Home"), WORK("Work"), FAX("Fax"), OTHER("Other");
+
+    private String description;
+
+    private PhoneNumberType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getValue() {
+        return name();
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setValue(String value) {
+    }
 }
